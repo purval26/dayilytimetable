369803639702d20c79e24c6d22025bfa8ad3de41
purval26 <purvalradadiya@gmail.com>
2024-11-17 10:04:39 +0530

    ADD

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ const selectElement = document.getElementById("options");
 // selectElement.addEventListener("change", () => {
 //     opt.style.display = "none";
 //   });
-
 async function fetchapi() {
     try {
         const response = await fetch(API);
         data = await response.json();
         displayData(data); // This will update the display with new data
+        // checkdata();
         links();
     } catch (err) {
         console.log("Something went wrong", err);
@@ -50,16 +50,16 @@ selectElement.addEventListener("change", () => {
 });
 
 function prevtable() {
-    if (i+1<data.length) {
+    if (i + 1 < data.length) {
         i = i + 1;
-        
+
         fetchapi();
     }
 }
 function nexttable() {
-    if (i>0) {
+    if (i > 0) {
         i = i - 1;
-        
+
         fetchapi();
     }
 }
@@ -73,19 +73,50 @@ function displayData(data) {
     const table = `
     <table>
     <tr><th class="date">${data[i].date}</th></tr>
-    <tr><td class="1">${data[i].lec1}</td></tr>
-    <tr><td class="2">${data[i].lec2}</td></tr>
-    <tr><td class="3">${data[i].lec3}</td></tr>
-    <tr><td class="4">${data[i].lec4}</td></tr>
-    <tr><td class="5">${data[i].lec5}</td></tr>
-    <tr><td class="6">${data[i].lec6}</td></tr>
-    <tr><td class="7">${data[i].lec7}</td></tr>
+    <tr><td class="1 lect1">${data[i].lec1}</td></tr>
+    <tr><td class="2 lect2">${data[i].lec2}</td></tr>
+    <tr><td class="3 lect3">${data[i].lec3}</td></tr>
+    <tr><td class="4 lect4">${data[i].lec4}</td></tr>
+    <tr><td class="5 lect5">${data[i].lec5}</td></tr>
+    <tr><td class="6 lect6">${data[i].lec6}</td></tr>
+    <tr><td class="7 lect7">${data[i].lec7}</td></tr>
     </table>
     <a class="link link1" id="link1" href="./hw.html">Today's Homework & Next Exam Syllabus</a>
     <a class="link" id="link2" href="./bp.html" style="display: none;">Download SA1 Blueprints Here!</a>
     `;
     center.innerHTML = table;
+    const lect1 = document.querySelector(".lect1");
+    const lect2 = document.querySelector(".lect2");
+    const lect3 = document.querySelector(".lect3");
+    const lect4 = document.querySelector(".lect4");
+    const lect5 = document.querySelector(".lect5");
+    const lect6 = document.querySelector(".lect6");
+    const lect7 = document.querySelector(".lect7");
+    checkdata(lect1, lect2, lect3, lect4, lect5, lect6, lect7);
 };
+function checkdata(lect1, lect2, lect3, lect4, lect5, lect6, lect7) {
+    if (data[i].lec1 === "") {
+        lect1.classList.add("hide");
+    }
+    if (data[i].lec2 === "") {
+        lect2.classList.add("hide");
+    } 
+    if (data[i].lec3 === "") {
+        lect3.classList.add("hide");
+    } 
+    if (data[i].lec4 === "") {
+        lect4.classList.add("hide");
+    } 
+    if (data[i].lec5 === "") {
+        lect5.classList.add("hide");
+    }
+    if (data[i].lec6 === "") {
+        lect6.classList.add("hide");
+    }
+    if (data[i].lec7 === "") {
+        lect7.classList.add("hide");
+    }
+}
 async function links() {
     const link = document.querySelector(".link1");
     console.log("Link Element:", link); // Check if the link is found
@@ -101,7 +132,7 @@ async function links() {
     }
 }
 // links();
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
     // Perform install steps
     var CACHE_NAME = '12-B/D Time Table';
     var urlsToCache = [
@@ -116,7 +147,7 @@ self.addEventListener('install', function(event) {
 
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(function(cache) {
+            .then(function (cache) {
                 console.log('Opened cache');
                 return cache.addAll(urlsToCache);
             })
