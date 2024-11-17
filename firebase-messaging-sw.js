2441db125668346509bc0623e5fa087aea4aed70
purval26 <purvalradadiya@gmail.com>
2024-11-17 12:43:49 +0530

    not

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -27,13 +27,3 @@ messaging.onBackgroundMessage((payload) => {
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/firebase-messaging-sw.js')
-      .then(function(registration) {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch(function(error) {
-        console.error('Service Worker registration failed:', error);
-      });
-  }
-  
--- script.js
+++ script.js
@@ -155,12 +155,12 @@ self.addEventListener('install', function (event) {
             })
         );
 });
-        // if ('serviceWorker' in navigator) {
-        //     navigator.serviceWorker.register('./firebase-messaging-sw.js')
-        //       .then((registration) => {
-        //         console.log('Service Worker registered:', registration);
-        //       })
-        //       .catch((error) => {
-        //         console.error('Service Worker registration failed:', error);
-        //       });
-        //   }
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('./firebase-messaging-sw.js')
+              .then((registration) => {
+                console.log('Service Worker registered:', registration);
+              })
+              .catch((error) => {
+                console.error('Service Worker registration failed:', error);
+              });
+          }
