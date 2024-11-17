29738daea1591fce204fc5e81fe74d0dea83c7d2
purval26 <purvalradadiya@gmail.com>
2024-11-17 11:27:14 +0530

    noy

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,8 @@
 importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging.js');
 
-// Initialize Firebase in the service worker
-const firebaseConfig = {
+// Firebase configuration
+firebase.initializeApp({
   apiKey: "AIzaSyBMXCTJLTbKpZB6p68EnVYIqMFpQB4yRZQ",
   authDomain: "txpurval.firebaseapp.com",
   projectId: "txpurval",
@@ -10,18 +10,19 @@ const firebaseConfig = {
   messagingSenderId: "3925062439",
   appId: "1:3925062439:web:74e54ef861cb0713b9af02",
   measurementId: "G-PQERDT8CCT"
-};
+});
 
-firebase.initializeApp(firebaseConfig);
+// Retrieve Firebase Messaging instance
 const messaging = firebase.messaging();
 
+// Handle background messages
 messaging.onBackgroundMessage((payload) => {
   console.log('Received background message: ', payload);
 
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.icon
+    icon: payload.notification.icon,
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
