c5d2455e0454458d9dde39f469b7f7fdf06986d8
purval26 <purvalradadiya@gmail.com>
2024-11-17 12:49:45 +0530

    not

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging-compat.js');
 
 // Firebase configuration
 firebase.initializeApp({
