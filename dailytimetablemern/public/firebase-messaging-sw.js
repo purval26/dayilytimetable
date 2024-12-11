51e016ac2ca8c35734f600e4e6980bf35a3da851
purval26 <purvalradadiya@gmail.com>
2024-12-11 19:02:21 +0530

    add

---
+++ dailytimetablemern/public/firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+// This is the Firebase Messaging service worker.
+importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBMXCTJLTbKpZB6p68EnVYIqMFpQB4yRZQ",
+  authDomain: "txpurval.firebaseapp.com",
+  projectId: "txpurval",
+  storageBucket: "txpurval.firebasestorage.app",
+  messagingSenderId: "3925062439",
+  appId: "1:3925062439:web:74e54ef861cb0713b9af02",
+});
+
+const messaging = firebase.messaging();
+
+// Handle background messages
+messaging.onBackgroundMessage(function (payload) {
+  console.log('Received background message: ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
